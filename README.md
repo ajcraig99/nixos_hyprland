529a9da5332440d3f5044e5254886eaaa9bdcb67
arron <ajcraig99@gmail.com>
2025-11-23 21:48:53 +1300

    cleaned out dir

--- README.md
+++
@@ -1,1 +0,0 @@
-# nixos_hyprland
--- apps/rofi/launchers/type-1/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dir="$HOME/.config/rofi/launchers/type-1"
-theme="style-1"
-
-## Execute
-rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
